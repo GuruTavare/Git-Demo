bb11a0c7d468f14dccbcaf69ade86454a2c17889
GuruTavare <gurutavare192@gmail.com>
2023-09-27 08:43:02 +0530

    Second commit

--- Sample.java
+++ Sample.java
@@ -1,5 +1,7 @@
 class Sample{
 	public staic void main(String[] args){
-		Sysytem.out.println("Welcome !!! to git bash");
+		System.out.println("Welcome !!! to git bash");
+		System.out.println("Welcome !!! to git bash");
+		System.out.println("Welcome !!! to git bash");
 	}
 }
